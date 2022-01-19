870a6169be7b8db1a537bb88eb7de68605f45117
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-01-19 21:13:38 +0530

    first part of keeper app done

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+import Note from "./note.jsx";
+
+function App(){
+  return <div>
+    <Header />
+    <Footer />
+    <Note />
+    </div>
+}
+export default App;
---
+++ src/note.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Note(){
+  return <div className="note">
+      <h1>Title of content</h1>
+      <p>Content here</p>
+  </div>
+}
+export default Note;
