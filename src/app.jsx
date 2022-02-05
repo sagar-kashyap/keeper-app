393bab8633d6944f998d50eabada702918f2cacb
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-02-06 02:54:28 +0530

    some error

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
 import Note from "./note.jsx";
-import notes from "./notes.jsx"
+import notes from "./notes.jsx";
 
 
 function App(){
